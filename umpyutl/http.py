b9ce64cad6c59e2ed23f9d0bf3d9e17f9c20a440
Anthony Whyte <anthwhyte@gmail.com>
2022-01-07 18:12:34 -0500

    Add package files

---
+++ umpyutl/http.py
@@ -0,0 +1,41 @@
+import requests as req
+from typing import Optional, Union
+
+
+def get_resource(
+    url: str,
+    params: Optional[dict] = None,
+    timeout: int = 10
+    ) -> req.Response:
+    """Returns a response object.
+
+    Parameters:
+        url (str): a url that specifies the resource.
+        params (dict): optional dictionary of querystring arguments.
+        timeout (int): timeout value in seconds
+
+    Returns:
+        Response: contains a server’s response to an HTTP request.
+    """
+    if params:
+        return req.get(url, params, timeout=timeout)
+    else:
+        return req.get(url, timeout=timeout)
+
+
+def get_resource_json(
+    url: str,
+    params: Optional[dict] = None,
+    timeout: int = 10
+    ) -> Union[dict, list]:
+    """Returns a response object decoded into a dictionary or list.
+
+    Parameters:
+        url (str): a url that specifies the resource.
+        params (dict): optional dictionary of querystring arguments.
+        timeout (int): timeout value in seconds
+
+    Returns:
+        dict | list: dictionary or list representation of the decoded JSON.
+    """
+    return get_resource(url, params, timeout=timeout).json()
---
+++ umpyutl/reader.py
@@ -0,0 +1,114 @@
+import csv
+import json
+from typing import List, Union
+
+
+def read_csv(
+    filepath: str,
+    encoding: str = 'utf-8',
+    newline: str = '',
+    delimiter: str = ','
+    ) -> List[list]:
+    """
+    Reads a CSV file, parsing row values per the provided delimiter. Returns a list of lists,
+    wherein each nested list represents a single row from the input file.
+
+    WARN: If a byte order mark (BOM) is encountered at the beginning of the first line of decoded
+    text, call < read_csv > and pass 'utf-8-sig' as the < encoding > argument.
+
+    WARN: If newline='' is not specified, newlines '\n' or '\r\n' embedded inside quoted fields
+    may not be interpreted correctly by the csv.reader.
+
+    Parameters:
+        filepath (str): The location of the file to read
+        encoding (str): name of encoding used to decode the file
+        newline (str): specifies replacement value for newline '\n'
+                       or '\r\n' (Windows) character sequences
+        delimiter (str): delimiter that separates the row values
+
+    Returns:
+        list: a list of nested "row" lists
+    """
+    with open(filepath, 'r', encoding=encoding, newline=newline) as file_obj:
+        data = []
+        reader = csv.reader(file_obj, delimiter=delimiter)
+        for row in reader:
+            data.append(row)
+
+        return data
+
+
+def read_csv_to_dicts(
+    filepath: str,
+    encoding: str ='utf-8',
+    newline: str ='',
+    delimiter: str =','
+    ) -> List[dict]:
+    """Accepts a file path, creates a file object, and returns a list of dictionaries
+    that represent the row values using the cvs.DictReader().
+
+    Parameters:
+        filepath (str): path to file
+        encoding (str): name of encoding used to decode the file
+        newline (str): specifies replacement value for newline '\n'
+                       or '\r\n' (Windows) character sequences
+        delimiter (str): delimiter that separates the row values
+
+    Returns:
+        list: nested dictionaries representing the file contents
+     """
+    with open(filepath, 'r', newline=newline, encoding=encoding) as file_obj:
+        data = []
+        reader = csv.DictReader(file_obj, delimiter=delimiter)
+        for line in reader:
+            data.append(line) # OrderedDict()
+            # data.append(dict(line)) # convert OrderedDict() to dict
+
+        return data
+
+
+def read_file(
+    filepath: str,
+    encoding: str = 'utf-8',
+    strip: bool = True
+    ) -> List[str]:
+    """Read text file line by line. Remove whitespace and trailing newline
+    escape character.
+
+    Parameters:
+        filepath (str): path to file
+        encoding (str): name of encoding used to decode the file.
+        strip (bool): remove white space, newline escape characters
+
+    Returns
+        list: list of lines in file
+    """
+    with open(filepath, 'r', encoding=encoding) as file_obj:
+        if strip:
+            data = []
+            for line in file_obj:
+                # data.append(line) # includes trailing newline '\n'
+                data.append(line.strip()) # strip leading/trailing whitespace including '\n'
+            return data
+
+            # return [line.strip() for line in file_obj] # list comprehension (single line)
+        else:
+            return file_obj.readlines() # list
+
+
+def read_json(
+    filepath: str,
+    encoding: str = 'utf-8'
+    ) -> Union[dict, list]:
+    """Reads a JSON document, decodes the file content, and returns a list or dictionary if
+    provided with a valid filepath.
+
+    Parameters:
+        filepath (str): path to file
+        encoding (str): name of encoding used to decode the file
+
+    Returns:
+        dict | list: dict or list representations of the decoded JSON document
+    """
+    with open(filepath, 'r', encoding=encoding) as file_obj:
+        return json.load(file_obj)
