6514f7a15d1b90791be0332ec59e2e0166f14fcc
Anthony Whyte <anthwhyte@gmail.com>
2022-04-18 22:46:57 -0400

    Add http tests

--- tests/test_pkg.py
+++ tests/test_pkg.py
@@ -1,5 +1,6 @@
 import inspect
 import re
+import requests
 import sys
 import unittest
 
@@ -22,27 +23,27 @@ class UmpyUtlTest(unittest.TestCase):
         """read.read_csv test"""
 
         planets = read.read_csv(f"{self.fixtures_path}/wookieepedia_planets.csv")
-        self.assertIsInstance(planets, list, "Error: planets is not a list.")
+        self.assertIsInstance(planets, list, 'Error: planets is not a list.')
 
     def test_02_read_csv_to_dicts(self):
         """read.read_csv_to_dicts test"""
 
         starships = read.read_csv_to_dicts(f"{self.fixtures_path}/wookieepedia_starships.csv")
-        self.assertIsInstance(starships, list, "Error: starships is not a list.")
-        self.assertIsInstance(starships[0], dict, "Error: starships[0] is not a dict.")
+        self.assertIsInstance(starships, list, 'Error: starships is not a list.')
+        self.assertIsInstance(starships[0], dict, 'Error: starships[0] is not a dict.')
 
     def test_03_read_file(self):
         """read.read_file test"""
 
         speech = read.read_file(f"{self.fixtures_path}/mandela-rivonia_trial-verbatim.txt")
-        self.assertIsInstance(speech, list, "Error: speech is not a list.")
+        self.assertIsInstance(speech, list, 'Error: speech is not a list.')
         self.assertEqual(speech[2], 'Transcript', "Error: speech[2] does not equal 'Transcript'")
 
     def test_04_read_json(self):
         """read.read_json test"""
 
         people = read.read_csv(f"{self.fixtures_path}/wookieepedia_people.json")
-        self.assertIsInstance(people, list, "Error: people is not a list.")
+        self.assertIsInstance(people, list, 'Error: people is not a list.')
 
     def test_05_write_csv(self):
         """write.write_csv test"""
@@ -67,10 +68,18 @@ class UmpyUtlTest(unittest.TestCase):
         # Read file
         vax_counties = read.read_csv(filepath)
 
-        self.assertIsInstance(vax_counties, list, "Error: vax_counties is not a list.")
-        self.assertIsInstance(vax_counties[-1], list, "Error: vax_counties[-1] is not a list.")
-        self.assertEqual(vax_counties, fxt_vax_counties, 'Error: file contents do not match fixture value')
-
+        self.assertIsInstance(
+            vax_counties,
+            list,
+            'Error: vax_counties is not a list.')
+        self.assertIsInstance(
+            vax_counties[-1],
+            list,
+            'Error: vax_counties[-1] is not a list.')
+        self.assertEqual(
+            vax_counties,
+            fxt_vax_counties,
+            'Error: file contents do not match fixture value')
 
     def test_06_write_dicts_to_csv(self):
         """write.write_dicts_to_csv test"""
@@ -102,9 +111,18 @@ class UmpyUtlTest(unittest.TestCase):
         # Read file
         passengers = read.read_csv_to_dicts(filepath)
 
-        self.assertIsInstance(passengers, list, "Error: passengers is not a list.")
-        self.assertIsInstance(passengers[-1], dict, "Error: passengers[-1] is not a dict.")
-        self.assertEqual(passengers, fxt_passengers, 'Error: file contents do not match fixture value')
+        self.assertIsInstance(
+            passengers,
+            list,
+            'Error: passengers is not a list.')
+        self.assertIsInstance(
+            passengers[-1],
+            dict,
+            'Error: passengers[-1] is not a dict.')
+        self.assertEqual(
+            passengers,
+            fxt_passengers,
+            'Error: file contents do not match fixture value')
 
     def test_07_write_file(self):
         """write.write_file test"""
@@ -126,10 +144,18 @@ class UmpyUtlTest(unittest.TestCase):
         # Read file
         opening_crawl = read.read_file(filepath)
 
-        self.assertIsInstance(opening_crawl, list, "Error: opening_crawl is not a list.")
-        self.assertIsInstance(opening_crawl[-1], str, "Error: opening_crawl[-1] is not a str.")
-        self.assertEqual(opening_crawl, fxt_opening_crawl, 'Error: file contents do not match fixture value')
-
+        self.assertIsInstance(
+            opening_crawl,
+            list,
+            'Error: opening_crawl is not a list.')
+        self.assertIsInstance(
+            opening_crawl[-1],
+            str,
+            'Error: opening_crawl[-1] is not a str.')
+        self.assertEqual(
+            opening_crawl,
+            fxt_opening_crawl,
+            'Error: file contents do not match fixture value')
 
     def test_08_write_json(self):
         """write.write_json test"""
@@ -213,9 +239,205 @@ class UmpyUtlTest(unittest.TestCase):
         # Read file
         crew_members = read.read_json(filepath)
 
-        self.assertIsInstance(crew_members, dict, "Error: crew_members is not a dict.")
-        self.assertIsInstance(crew_members['pilot'], dict, "Error: crew_members['pilot'] is not a dict.")
-        self.assertEqual(crew_members, fxt_crew_members, 'Error: file contents do not match fixture value')
+        self.assertIsInstance(
+            crew_members,
+            dict,
+            'Error: crew_members is not a dict.')
+        self.assertIsInstance(
+            crew_members['pilot'],
+            dict,
+            "Error: crew_members['pilot'] is not a dict.")
+        self.assertEqual(
+            crew_members,
+            fxt_crew_members,
+            'Error: file contents do not match fixture value')
+
+    def test_09_get_resource(self):
+        """write.get_resource test"""
+
+        fxt_chewie = {
+            'name': 'Chewbacca',
+            'height': '228',
+            'mass': '112',
+            'hair_color': 'brown',
+            'skin_color': 'unknown',
+            'eye_color': 'blue',
+            'birth_year': '200BBY',
+            'gender': 'male',
+            'homeworld': 'https://swapi.py4e.com/api/planets/14/',
+            'films': [
+                'https://swapi.py4e.com/api/films/1/',
+                'https://swapi.py4e.com/api/films/2/',
+                'https://swapi.py4e.com/api/films/3/',
+                'https://swapi.py4e.com/api/films/6/',
+                'https://swapi.py4e.com/api/films/7/'
+            ],
+            'species': [
+                'https://swapi.py4e.com/api/species/3/'
+            ],
+            'vehicles': [
+                'https://swapi.py4e.com/api/vehicles/19/'
+            ],
+            'starships': [
+                'https://swapi.py4e.com/api/starships/10/',
+                'https://swapi.py4e.com/api/starships/22/'
+            ],
+            'created': '2014-12-10T16:42:45.066000Z',
+            'edited': '2014-12-20T21:17:50.332000Z',
+            'url': 'https://swapi.py4e.com/api/people/13/'
+            }
+
+        response = http.get_resource('https://swapi.py4e.com/api/people/13/')
+
+        self.assertIsInstance(
+            response,
+            requests.Response,
+            'Error: response is not an instance of requests.Response')
+        self.assertIsInstance(
+            response.json(),
+            dict, 'Error: response is not a dict.')
+        self.assertEqual(
+            response.json(),
+            fxt_chewie,
+            'Error: decoded response does not match fixture dict')
+
+    def test_10_get_resource_params(self):
+        """write.get_resource with params test"""
+
+        fxt_response = {
+            'count': 1,
+            'next': None,
+            'previous': None,
+            'results': [
+                {
+                    'name': 'Han Solo',
+                    'height': '180',
+                    'mass': '80',
+                    'hair_color': 'brown',
+                    'skin_color': 'fair',
+                    'eye_color': 'brown',
+                    'birth_year': '29BBY',
+                    'gender': 'male',
+                    'homeworld': 'https://swapi.py4e.com/api/planets/22/',
+                    'films': [
+                        'https://swapi.py4e.com/api/films/1/',
+                        'https://swapi.py4e.com/api/films/2/',
+                        'https://swapi.py4e.com/api/films/3/',
+                        'https://swapi.py4e.com/api/films/7/'
+                    ],
+                    'species': [
+                        'https://swapi.py4e.com/api/species/1/'
+                    ],
+                    'vehicles': [],
+                    'starships': [
+                        'https://swapi.py4e.com/api/starships/10/',
+                        'https://swapi.py4e.com/api/starships/22/'
+                    ],
+                    'created': '2014-12-10T16:49:14.582000Z',
+                    'edited': '2014-12-20T21:17:50.334000Z',
+                    'url': 'https://swapi.py4e.com/api/people/14/'
+                }
+            ]
+        }
+
+        response = http.get_resource('https://swapi.py4e.com/api/people/', {'search': 'han solo'})
+
+        self.assertIsInstance(
+            response,
+            requests.Response,
+            'Error: response is not an instance of requests.Response')
+        self.assertIsInstance(
+            response.json(),
+            dict,
+            'Error: response is not a dict.')
+        self.assertEqual(
+            response.json(),
+            fxt_response,
+            'Error: decoded response does not match fixture response.')
+
+    def test_11_get_resource_json(self):
+        """write.get_resource_json test"""
+
+        fxt_m_falcon = {
+            'name': 'Millennium Falcon',
+            'model': 'YT-1300 light freighter',
+            'manufacturer': 'Corellian Engineering Corporation',
+            'cost_in_credits': '100000',
+            'length': '34.37',
+            'max_atmosphering_speed': '1050',
+            'crew': '4',
+            'passengers': '6',
+            'cargo_capacity': '100000',
+            'consumables': '2 months',
+            'hyperdrive_rating': '0.5',
+            'MGLT': '75',
+            'starship_class': 'Light freighter',
+            'pilots': [
+                'https://swapi.py4e.com/api/people/13/',
+                'https://swapi.py4e.com/api/people/14/',
+                'https://swapi.py4e.com/api/people/25/',
+                'https://swapi.py4e.com/api/people/31/'
+            ],
+            'films': [
+                'https://swapi.py4e.com/api/films/1/',
+                'https://swapi.py4e.com/api/films/2/',
+                'https://swapi.py4e.com/api/films/3/',
+                'https://swapi.py4e.com/api/films/7/'
+            ],
+            'created': '2014-12-10T16:59:45.094000Z',
+            'edited': '2014-12-20T21:23:49.880000Z',
+            'url': 'https://swapi.py4e.com/api/starships/10/'
+            }
+
+        m_falcon = http.get_resource_json('https://swapi.py4e.com/api/starships/10/')
+
+        self.assertIsInstance(
+            m_falcon,
+            dict, 'Error: response is not a dict.')
+        self.assertEqual(
+            m_falcon,
+            fxt_m_falcon,
+            'Error: decoded response does not match fixture dict')
+
+    def test_12_get_resource_json_params(self):
+        """write.get_resource_json with params test"""
+
+        fxt_response = {
+            'count': 1,
+            'next': None,
+            'previous': None,
+            'results': [
+                {
+                    'name': 'Hoth',
+                    'rotation_period': '23',
+                    'orbital_period': '549',
+                    'diameter': '7200',
+                    'climate': 'frozen',
+                    'gravity': '1.1 standard',
+                    'terrain': 'tundra, ice caves, mountain ranges',
+                    'surface_water': '100',
+                    'population': 'unknown',
+                    'residents': [],
+                    'films': [
+                        'https://swapi.py4e.com/api/films/2/'
+                    ],
+                    'created': '2014-12-10T11:39:13.934000Z',
+                    'edited': '2014-12-20T20:58:18.423000Z',
+                    'url': 'https://swapi.py4e.com/api/planets/4/'
+                }
+            ]
+        }
+
+        response = http.get_resource_json('https://swapi.py4e.com/api/planets/', {'search': 'hoth'})
+
+        self.assertIsInstance(
+            response,
+            dict,
+            'Error: response is not a dict.')
+        self.assertEqual(
+            response,
+            fxt_response,
+            'Error: decoded response does not match fixture response.')
 
 
 if __name__ == "__main__":
