e8e9fc982d3026cc92113c134b0b8c037bc09d3c
Anthony Whyte <anthwhyte@gmail.com>
2023-08-02 09:40:59 -0400

    Add py.typed for mypy

--- setup.py
+++ setup.py
@@ -6,23 +6,21 @@ with open("./README.md", "r") as fileobj:
 # Consider appending username to name in order to reduce possible package name collisions.
 setuptools.setup(
     name="umpyutl",
-    version="2.0.2",
+    version="2.0.3",
     author="Anthony Whyte",
     author_email="anthwhyte@gmail.com",
     description="Utility classes and functions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords='arwhyte, utilities, umpyutl',
+    keywords="arwhyte, utilities, umpyutl",
     url="https://github.com/umsi-arwhyte/umpyutl",
-    install_requires=[
-        'pyYAML',
-        'requests'
-        ],
-    packages=setuptools.find_packages(include=['umpyutl', 'umpyutl.*']),
+    install_requires=["pyYAML", "requests"],
+    package_data={"umpyutl": ["py.typed"]},
+    packages=setuptools.find_packages(include=["umpyutl", "umpyutl.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        ],
-    python_requires='>=3.10',
+    ],
+    python_requires=">=3.10",
 )
